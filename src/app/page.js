1abf0ff7e47a7caee6555abfd539e293d7a9b3b1
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-04-02 09:38:04 -0400

    Création et utilisation de classes utilitaires avec TailwindCSS

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 export default async function Home() {
   return (
-    <div className="">
+    <div className="u-main-container u-padding-content-container">
+      <h1 className="t-main-title">Restez à jour avec Axoria Blog</h1>
+      <p className="t-main-subtitle">Actualités tech et savoirs utiles</p>
     </div>
   );
 }
