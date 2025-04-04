7731e520407b58bd0539aa7f6b6aef1cc50135b1
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-04-04 17:53:58 -0400

    Création d'un modèle Post avec Mongoose

---
+++ src/lib/models/post.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import slugify from "slugify";
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    markdownArticle: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+postSchema.pre("save", async function (next) {
+  if (!this.slug && this.title) {
+    const titleSlug = slugify(this.title.trim(), { lower: true, strict: true });
+    let slugCandidate = titleSlug;
+
+    let slugExists = await mongoose.models.Post.findOne({
+      slug: slugCandidate,
+    });
+
+    if (slugExists) {
+      let counter = 1;
+
+      while (slugExists) {
+        slugCandidate = `${titleSlug}-${counter}`;
+        slugExists = await mongoose.models.Post.findOne({
+          slug: slugCandidate,
+        });
+        counter++;
+      }
+    }
+
+    this.slug = slugCandidate;
+  }
+
+  next();
+});
+
+export const Post = mongoose.models?.Post || mongoose.model("Post", postSchema);
