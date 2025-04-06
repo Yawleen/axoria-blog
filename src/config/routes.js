c6458d3d2f3f18f76c10eba9adca852f5086bdb8
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-04-06 17:53:09 -0400

    Ajout de routes d'authentification pour l'inscription et la connexion

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,3 +2,5 @@ export const HOME_ROUTE = "/";
 export const CATEGORIES_ROUTE = "/categories";
 export const DASHBOARD_CREATE_ROUTE = "/dashboard/create";
 export const ARTICLE_ROUTE = "/article";
+export const SIGN_UP_ROUTE = "/signup";
+export const SIGN_IN_ROUTE = "/signin";
