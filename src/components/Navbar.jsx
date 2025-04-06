083a6779762677bce6e9b9a342a61164009be14e
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-04-06 17:57:02 -0400

    Ajout des routes d'authentification à la Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import Link from "next/link"
-import { HOME_ROUTE, CATEGORIES_ROUTE, DASHBOARD_CREATE_ROUTE } from "@/config/routes"
+import { HOME_ROUTE, CATEGORIES_ROUTE, DASHBOARD_CREATE_ROUTE, SIGN_UP_ROUTE, SIGN_IN_ROUTE } from "@/config/routes"
 
 export default function Navbar() {
     return (
         <nav className="fixed w-full bg-slate-50 border-b border-b-zinc-300">
             <div className="u-main-container flex py-4">
-                <Link href={HOME_ROUTE} className="uppercase mr-2 text-zinc-900">Axoria</Link>
+                <Link href={HOME_ROUTE} className="uppercase mr-4 text-zinc-900">Axoria</Link>
                 <Link href={CATEGORIES_ROUTE} className="text-zinc-900 mr-auto">Catégories</Link>
                 <Link href={DASHBOARD_CREATE_ROUTE} className="text-zinc-900">Ajouter un article</Link>
+                <Link href={SIGN_UP_ROUTE} className="text-zinc-900">S'inscrire</Link>
+                <Link href={SIGN_IN_ROUTE} className="text-zinc-900 mr-2">Se connecter</Link>
             </div>
         </nav>
     )
