1132c94cbb6c1905c9d0acb877d782b3afbdb1d6
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-04-06 18:26:45 -0400

    Ajout d'un formulaire sur la page d'inscription

--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -1,5 +1,69 @@
+import Link from "next/link";
+
 export default function SignUpPage() {
   return (
-    <div>page</div>
-  )
+    <form className="max-w-md mx-auto my-36">
+      <label htmlFor="userName" className="f-label">
+        Nom ou pseudo
+      </label>
+      <input
+        className="f-auth-input"
+        type="text"
+        id="userName"
+        name="userName"
+        placeholder="Nom ou pseudo"
+        required
+      />
+
+      <label htmlFor="email" className="f-label">
+        E-mail
+      </label>
+      <input
+        className="f-auth-input"
+        type="email"
+        id="email"
+        name="email"
+        placeholder="E-mail"
+        required
+      />
+
+      <label htmlFor="password" className="f-label">
+        Mot de passe
+      </label>
+      <input
+        className="f-auth-input"
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Mot de passe"
+        required
+      />
+
+      <label htmlFor="passwordRepeat" className="f-label">
+        Confirmation du mot de passe
+      </label>
+      <input
+        className="f-auth-input"
+        type="password"
+        id="passwordRepeat"
+        name="passwordRepeat"
+        placeholder="Confirmation du mot de passe"
+        required
+      />
+
+      <button
+        className="w-full bg-indigo-500 hover:bg-indigo-700 text-white font-bold
+       py-3 px-4 my-10 rounded border-none"
+      >
+        Soumettre
+      </button>
+      <p className="hidden text-center mb-10"></p>
+      <Link
+        href="/signin"
+        className="underline text-blue-600 block text-center"
+      >
+        Vous avez déjà un compte ? Connectez-vous
+      </Link>
+    </form>
+  );
 }
