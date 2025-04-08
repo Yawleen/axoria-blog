37bc49f602e7f4d18230af9d4f5b6230ba3c2c27
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-04-07 21:06:04 -0400

    Ajout d'un formulaire sur la page de connexion

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -1,5 +1,37 @@
 export default function SignInPage() {
   return (
-    <div>page</div>
-  )
+    <form className="max-w-md mx-auto mt-36">
+      <label htmlFor="userName" className="f-label">
+        Pseudo
+      </label>
+      <input
+        type="text"
+        className="f-auth-input"
+        id="userName"
+        name="userName"
+        required
+        placeholder="Pseudo"
+      />
+
+      <label htmlFor="password" className="f-label">
+        Mot de passe
+      </label>
+      <input
+        type="password"
+        className="f-auth-input"
+        id="password"
+        name="password"
+        required
+        placeholder="Mot de passe"
+      />
+
+      <button
+        className="w-full bg-indigo-500 hover:bg-indigo-700 text-white font-bold
+     py-3 px-4 mt-6 mb-10 rounded border-none"
+      >
+        Soumettre
+      </button>
+      <p className="text-center mb-10"></p>
+    </form>
+  );
 }
