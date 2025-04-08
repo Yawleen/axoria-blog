c33145b65a04b0d2166ec3eb4c408b57d1e744a7
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-04-07 23:43:27 -0400

    Affichage des options d'authentification si l'utilisateur n'est pas connecté

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,25 @@
 import Link from "next/link"
 import { HOME_ROUTE, CATEGORIES_ROUTE, DASHBOARD_CREATE_ROUTE, SIGN_UP_ROUTE, SIGN_IN_ROUTE } from "@/config/routes"
+import { sessionInfo } from "@/lib/serverMethods/session/sessionMethods"
 
-export default function Navbar() {
+export default async function Navbar() {
+    const session = await sessionInfo();
+
     return (
         <nav className="fixed w-full bg-slate-50 border-b border-b-zinc-300">
             <div className="u-main-container flex py-4">
                 <Link href={HOME_ROUTE} className="uppercase mr-4 text-zinc-900">Axoria</Link>
                 <Link href={CATEGORIES_ROUTE} className="text-zinc-900 mr-auto">Catégories</Link>
-                <Link href={DASHBOARD_CREATE_ROUTE} className="text-zinc-900">Ajouter un article</Link>
-                <Link href={SIGN_UP_ROUTE} className="text-zinc-900 mr-2">S'inscrire</Link>
-                <Link href={SIGN_IN_ROUTE} className="text-zinc-900">Se connecter</Link>
+                {
+                    session.success ? (
+                        <Link href={DASHBOARD_CREATE_ROUTE} className="text-zinc-900">Ajouter un article</Link>
+                    ) : (
+                        <>
+                            <Link href={SIGN_UP_ROUTE} className="text-zinc-900 mr-2">S'inscrire</Link>
+                            <Link href={SIGN_IN_ROUTE} className="text-zinc-900">Se connecter</Link>
+                        </>
+                    )
+                }
             </div>
         </nav>
     )
