223455fca85bc34fe1a00b3f5b9957e8017230f5
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-04-07 22:24:44 -0400

    Gestion de l'envoi du formulaire de connexion

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -1,6 +1,38 @@
+"use client";
+
+import { login } from "@/lib/serverActions/session/sessionServerActions";
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
+import { HOME_ROUTE } from "@/config/routes";
+
 export default function SignInPage() {
+  const serverInfoRef = useRef(null);
+  const submitButtonRef = useRef(null);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    serverInfoRef.current.textContext = "";
+    submitButtonRef.current.disabled = true;
+
+    try {
+      const result = await login(formData);
+
+      if (result.success) {
+        router.push(HOME_ROUTE);
+      }
+    } catch (error) {
+      submitButtonRef.current.textContent = "Soumettre";
+      serverInfoRef.current.textContent = `${error.message}`;
+      submitButtonRef.current.disabled = false;
+    }
+  };
+
   return (
-    <form className="max-w-md mx-auto mt-36">
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-36">
       <label htmlFor="userName" className="f-label">
         Pseudo
       </label>
@@ -24,14 +56,18 @@ export default function SignInPage() {
         required
         placeholder="Mot de passe"
       />
+      <p
+        ref={serverInfoRef}
+        className="font-semibold text-red-500 text-center"
+      ></p>
 
       <button
+        ref={submitButtonRef}
         className="w-full bg-indigo-500 hover:bg-indigo-700 text-white font-bold
      py-3 px-4 mt-6 mb-10 rounded border-none"
       >
         Soumettre
       </button>
-      <p className="text-center mb-10"></p>
     </form>
   );
 }
