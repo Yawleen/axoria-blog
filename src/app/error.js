5a4310cd22ae595bbfb5778bb529ad01bf6e3d49
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-04-10 15:36:55 -0400

    Création d'une page d'erreur générique

---
+++ src/app/error.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { HOME_ROUTE } from "@/config/routes";
+import Link from "next/link";
+
+export default function error() {
+  return (
+    <div className="pt-44 text-center">
+      <h1 className="text-4xl mb-4">Une erreur serveur est survenue.</h1>
+      <Link href={HOME_ROUTE} className="underline">
+        Retourner à la page d'accueil
+      </Link>
+    </div>
+  );
+}
