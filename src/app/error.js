908ae628d7a64b1d0c626f7f80bed1b8ec0cff11
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-04-29 23:08:31 -0400

    Mise à jour de la page d'erreur

--- src/app/error.js
+++ src/app/error.js
@@ -3,7 +3,7 @@
 import { HOME_ROUTE } from "@/config/routes";
 import Link from "next/link";
 
-export default function error() {
+export default function Error() {
   return (
     <div className="pt-44 text-center">
       <h1 className="text-4xl mb-4">Une erreur serveur est survenue.</h1>
