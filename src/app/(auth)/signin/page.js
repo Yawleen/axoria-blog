38fdc3b4b5159cf287b353a38cfbaa48aa13ff41
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-05-01 21:53:46 -0400

    Utilisation d'un toast pour afficher les messages d'erreur sur la page de connexion

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -5,10 +5,10 @@ import { useRouter } from "next/navigation";
 import { useRef } from "react";
 import { HOME_ROUTE } from "@/config/routes";
 import { useAuth } from "@/app/AuthContext";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function SignInPage() {
   const { isAuthenticated, setIsAuthenticated } = useAuth();
-  const serverInfoRef = useRef(null);
   const submitButtonRef = useRef(null);
   const router = useRouter();
 
@@ -17,8 +17,6 @@ export default function SignInPage() {
 
     const formData = new FormData(e.target);
 
-    serverInfoRef.current.textContext = "";
-    serverInfoRef.current.classList.add("hidden");
     submitButtonRef.current.disabled = true;
     submitButtonRef.current.textContent = "Connexion en cours...";
 
@@ -40,8 +38,9 @@ export default function SignInPage() {
         return;
       }
     } catch (error) {
-      serverInfoRef.current.textContent = `${error.message}`;
-      serverInfoRef.current.classList.remove("hidden");
+      toast.error(error.message, {
+        removeDelay: 3000,
+      });
       submitButtonRef.current.textContent = "Soumettre";
       submitButtonRef.current.disabled = false;
     }
@@ -50,46 +49,43 @@ export default function SignInPage() {
       ...isAuthenticated,
       loading: false,
     });
-
-    window.scrollTo(0, document.body.scrollHeight);
   };
 
   return (
-    <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-36">
-      <label htmlFor="userName" className="f-label">
-        Pseudo
-      </label>
-      <input
-        type="text"
-        className="f-auth-input"
-        id="userName"
-        name="userName"
-        required
-        placeholder="Pseudo"
-      />
+    <>
+      <Toaster />
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-36">
+        <label htmlFor="userName" className="f-label">
+          Pseudo
+        </label>
+        <input
+          type="text"
+          className="f-auth-input"
+          id="userName"
+          name="userName"
+          required
+          placeholder="Pseudo"
+        />
 
-      <label htmlFor="password" className="f-label">
-        Mot de passe
-      </label>
-      <input
-        type="password"
-        className="f-auth-input"
-        id="password"
-        name="password"
-        required
-        placeholder="Mot de passe"
-      />
-      <p
-        ref={serverInfoRef}
-        className="hidden font-semibold text-red-500 text-center"
-      ></p>
-      <button
-        ref={submitButtonRef}
-        className="w-full bg-indigo-500 hover:bg-indigo-700 text-white font-bold
+        <label htmlFor="password" className="f-label">
+          Mot de passe
+        </label>
+        <input
+          type="password"
+          className="f-auth-input"
+          id="password"
+          name="password"
+          required
+          placeholder="Mot de passe"
+        />
+        <button
+          ref={submitButtonRef}
+          className="w-full bg-indigo-500 hover:bg-indigo-700 text-white font-bold
      py-3 px-4 mt-6 mb-10 rounded border-none"
-      >
-        Soumettre
-      </button>
-    </form>
+        >
+          Soumettre
+        </button>
+      </form>
+    </>
   );
 }
